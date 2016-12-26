10efea2b93c4346390d29367032296ca5eba3855
Canta <canta@canta.com.ar>
2016-12-25 21:37:58 -0300

    Agregada una gestión rústica de errores y posibilidad de continuar un backup.

--- backup_posts_from_list.py
+++ backup_posts_from_list.py
@@ -7,9 +7,19 @@ import math
 from bs4 import BeautifulSoup
 import json
 import time
+import os
 
 def debug(texto):
   print "[" + str(time.strftime("%H:%M:%S")) + "] - " + str(texto)
+
+def yes_no(texto):
+    reply = str(raw_input(texto+' (y/n): ')).lower().strip()
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
+    else:
+      return yes_no(texto)
 
 print "Nombre de usuario: "
 username  = raw_input()
@@ -41,30 +51,56 @@ links.sort(key=lambda x: int(x["post_id"]), reverse=False)
 actual = 0
 total  = len(links)
 
+if not os.path.isdir("./posts_backup"):
+  os.makedirs("./posts_backup")
+
+files  = [f for f in os.listdir("./posts_backup") if os.path.isfile(os.path.join("./posts_backup", f))]
+files.sort()
+
+if len(files) > 0 :
+  ultimo = files[ len(files) - 1 ].split(".")[1]
+  if yes_no("Continuar desde " + ultimo + "" ):
+    while str( links[actual]["post_id"] ) != ultimo :
+      actual = actual + 1
+
+
 while actual < total:
   debug("Procesando #" + str(links[actual]["post_id"]) + " (" + str(actual+1) + "/" + str(total) + "): " + links[actual]["url"] )
 
-  response          = session.get( forum + links[actual]["url"].replace("./","/") )
-  tmp               = {}
-  soup              = BeautifulSoup( response.text.encode("utf-8") , "lxml")
-  tmp["post"]       = soup.select("#p" + str(links[actual]["post_id"]) )[0]
-  content           = tmp["post"].select("#postdiv" + str(links[actual]["post_id"]))[0]
-  tmp["text"]       = content.text
-  tmp["html"]       = unicode( content )
-  tmp["post"]       = unicode( tmp["post"] )
-  # voy a buscar el bbcode
-  if "quick-edit-icon" in tmp["post"]:
-    payload           = {'post_id': str(links[actual]["post_id"]) }
-    response          = session.post(forum + "quickedit.php", data=payload)
-    response.encoding = "utf-8"
-    soup2             = BeautifulSoup("<html><body>" + response.text.encode("utf-8") + "</body></html>", "lxml")
-    tmp["bbcode"]     = soup2.select("#quickedit-textarea")[0].contents
-  
-  output  = json.dumps(tmp, indent = 4, ensure_ascii=False , sort_keys = True)
-  outfile = open( "./posts_backup/" + str(author_id) + "." + str(links[actual]["post_id"]) + ".txt", 'w')
-  outfile.write(output.encode("utf-8"))
-  outfile.close()
-  actual = actual + 1
+  try:
+    response          = session.get( forum + links[actual]["url"].replace("./","/") )
+    tmp               = {}
+    soup              = BeautifulSoup( response.text.encode("utf-8") , "lxml")
+    tmp["post"]       = soup.select("#p" + str(links[actual]["post_id"]) )[0]
+    content           = tmp["post"].select("#postdiv" + str(links[actual]["post_id"]))[0]
+    tmp["text"]       = content.text
+    tmp["html"]       = unicode( content )
+    tmp["post"]       = unicode( tmp["post"] )
+    # voy a buscar el bbcode
+    if "quick-edit-icon" in tmp["post"]:
+      payload           = {'post_id': str(links[actual]["post_id"]) }
+      response          = session.post(forum + "quickedit.php", data=payload)
+      response.encoding = "utf-8"
+      soup2             = BeautifulSoup("<html><body>" + response.text.encode("utf-8") + "</body></html>", "lxml")
+      tmp["bbcode"]     = soup2.select("#quickedit-textarea")[0].contents
+    
+    output  = json.dumps(tmp, indent = 4, ensure_ascii=False , sort_keys = True)
+    outfile = open( "./posts_backup/" + str(author_id) + "." + str(links[actual]["post_id"]) + ".txt", 'w')
+    outfile.write(output.encode("utf-8"))
+    outfile.close()
+    tries = 0
+    actual = actual + 1
+  except Exception, e:
+    debug("Error: " + str(e))
+    if tries < 3 :
+      tries = tries + 1
+    else:
+      debug("Salteándoselo por repetición de errores.")
+      errs   = open("errores.txt", "a")
+      errs.write( str(links[actual]["post_id"]).encode("utf-8") + " : " + unicode(e) + "\n")
+      errs.close()
+      tries  = 0
+      actual = actual + 1
 
 response  = session.get( forum + "/ucp.php?mode=logout")
 
